862d1a0e48422cc047eed95ef5ee929bb427fa54
Eugene Ovcharenko <me@eugeneovcharenko.com>
2024-07-16 10:25:29 -0700

    Initial commit

---
+++ Assets/PassthroughProjectionSurface.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public static class PassthroughProjectionSurface
+{
+    // [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    // private static void AfterSceneLoad()
+    // {
+    //     GameObject ovrCameraRig = GameObject.Find("OVRCameraRig");
+    //     OVRPassthroughLayer layer = ovrCameraRig.GetComponent<OVRPassthroughLayer>();
+    //     layer.AddSurfaceGeometry(gameObject, false);
+    //
+    //     // Disable the mesh renderer to avoid rendering the surface within Unity
+    //     MeshRenderer mr = GetComponent<MeshRenderer>();
+    //     if (mr)
+    //     {
+    //         mr.enabled = false;
+    //     }
+    // }
+
+}
